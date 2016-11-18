23e26e4cc9b46ac6011be670da5ba5ab8e763016
Mike Sinkovsky <msink@permonline.ru>
2016-11-18 17:55:04 +0500

    avian cross-compilation

--- build.bat
+++ build.bat
@@ -1,9 +1,14 @@
 @echo off
-rem	windows-64,windows-32,linux-64,linux-32,linux-arm32,linux-arm64,macosx-32,macosx-64
-for %%A in (windows-64,windows-32,linux-64,linux-32) do (
+
+rem	native,windows-64,windows-32,linux-64,linux-32,linux-arm-32,linux-arm-64,macosx-32,macosx-64
+for %%A in (native,windows-64,windows-32,linux-64,linux-32,linux-arm-32) do (
   if not exist build\%%A (
     mkdir build\%%A
-    meson --cross-file toolchains\%%A.txt build\%%A
+    if %%A == native (
+      meson build\%%A
+    ) else (
+      meson --cross-file toolchains\%%A.txt build\%%A
+    )
   )
-  ninja -v -C build/%%A
+  ninja -C build/%%A
 )
---
+++ hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void __cxa_pure_virtual()
+{
+	while (1);
+}
+
+int main()
+{
+	printf("Hello world!");
+	return 0;
+}
